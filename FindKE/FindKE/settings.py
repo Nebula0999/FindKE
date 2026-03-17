c91988e5198d4052983fbd160ad8e5e46a407cbe
Allan <nabuleallan1@gmail.com>
2026-03-17 11:23:49 +0300

    updated readme

--- FindKE/FindKE/settings.py
+++ FindKE/FindKE/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://findke.onrender.com']
 
 
 # Application definition
